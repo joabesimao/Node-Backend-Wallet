77d0758732cac42941f3c0e2c8119ab37d42368d
joabesimao <simaojoabecosta@gmail.com>
2025-05-28 16:14:02 -0300

    feat: ensure create usecase load-by-id Position

--- src/domain/usescases/position/load-position.ts
+++ src/domain/usescases/position/load-position.ts
@@ -3,3 +3,7 @@ import { Position } from "../../models/position-model/position";
 export interface LoadPosition {
   load(): Promise<Position[]>;
 }
+
+export interface LoadPositionById{
+  loadById(id:number):Promise<Position>
+}
