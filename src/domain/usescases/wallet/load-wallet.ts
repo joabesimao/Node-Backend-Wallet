b55df14b073ed729c084700379fbbc09b33f0732
joabesimao <simaojoabecosta@gmail.com>
2025-05-28 17:20:54 -0300

    feat: ensure create usecase load-wallet

--- src/domain/usescases/wallet/load-wallet.ts
+++ src/domain/usescases/wallet/load-wallet.ts
@@ -3,3 +3,7 @@ import { Wallet } from "../../models/wallet-model/wallet";
 export interface LoadWallet {
   load(): Promise<Wallet[]>;
 }
+
+export interface LoadWalletById {
+  loadById(id: number): Promise<Wallet>;
+}
