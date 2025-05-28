5e852c474c05dd5f7056cb8c439214df318fd579
joabesimao <simaojoabecosta@gmail.com>
2025-05-28 16:07:02 -0300

    feat: ensure create usecase load-by-id client

--- src/domain/usescases/client/load-client.ts
+++ src/domain/usescases/client/load-client.ts
@@ -3,3 +3,7 @@ import { Client } from "../../models/client-model/client";
 export interface LoadClient {
   load(): Promise<Client[]>;
 }
+
+export interface LoadClientById {
+  loadOne(id: number): Promise<Client>;
+}
