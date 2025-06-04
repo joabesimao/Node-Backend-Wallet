91bea9bed913a958e50e202d6e1cf08d1ccb0da4
Joabe Simão <simaojoabecosta@gmail.com>
2025-06-04 20:50:14 -0300

    feat: ensure add Wallet on success

---
+++ src/data/usescases/wallet-usecases/db-add-wallet.ts
@@ -0,0 +1,14 @@
+import { Wallet } from "../../../domain/models/wallet-model/wallet";
+import {
+  AddWallet,
+  AddWalletModel,
+} from "../../../domain/usescases/wallet/add-wallet";
+import { AddWalletRepository } from "../../protocols/db/wallet/add-wallet";
+
+export class DbAddWallet implements AddWallet {
+  constructor(private readonly addWalletRepository: AddWalletRepository) {}
+  async add(wallet: AddWalletModel): Promise<Wallet> {
+    const walletAddict = await this.addWalletRepository.add(wallet);
+    return walletAddict;
+  }
+}
