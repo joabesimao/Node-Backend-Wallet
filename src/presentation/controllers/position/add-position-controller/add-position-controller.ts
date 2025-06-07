39754cc1bf1eec2c437a15db208ff97d684d4be8
Joabe Simão <simaojoabecosta@gmail.com>
2025-06-07 14:21:51 -0300

    feat: ensure add position

---
+++ src/presentation/controllers/position/add-position-controller/add-position-controller.ts
@@ -0,0 +1,17 @@
+import { AddPosition } from "../../../../domain/usescases/position/add-position";
+import { ok, serverError } from "../../../helpers/http/http-helper";
+import { Controller } from "../../../protocols/controller/controller";
+import { HttpRequest, HttpResponse } from "../../../protocols/http/http";
+
+export class AddPositionController implements Controller {
+  constructor(private readonly addPosition: AddPosition) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const addNewPosition = await this.addPosition.add(httpRequest.body);
+      return ok(addNewPosition);
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+}
