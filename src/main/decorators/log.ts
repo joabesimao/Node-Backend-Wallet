3be02099e5d38a0c3bb058f8a756b1dc44816366
Joabe Simão <simaojoabecosta@gmail.com>
2025-06-12 14:08:29 -0300

    feat: ensure create log-decorator

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,19 @@
+import { Controller } from "../../presentation/protocols/controller/controller";
+import {
+  HttpRequest,
+  HttpResponse,
+} from "../../presentation/protocols/http/http";
+
+export class LogControllerDecorator implements Controller {
+  constructor(
+    private readonly controller: Controller,
+    private readonly logErrorRepository: LogErrorRepository
+  ) {}
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const result = await this.controller.handle(httpRequest);
+    if (result.statusCode === 500) {
+      await this.logErrorRepository.log(result.body.stack);
+    }
+    return result;
+  }
+}
